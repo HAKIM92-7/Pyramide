80724b5843094cf09c15e5839c3d5ced0a414f63
HAKIM <hakim@hakim-HP-EliteBook-8440p.com>
2020-02-20 16:31:08 +0100

    ajouter des commentaires

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 
-let Mypage=document.querySelector('html');
-let storednumber;
+let Mypage=document.querySelector('html');   /*appeler la page html*/
+let storednumber;                  /*déclarer un nombre*/
 
-const nombreetage =() => 
+const nombreetage =() =>                                  /* fonction qui demande au visiteur le nombre d etage*/ 
 {let nombresaisi=prompt('veuillez donner le nombre d etage du pyramide');  
   localStorage.setItem('N',nombresaisi) ;}
 
@@ -13,23 +13,25 @@ const nombreetage =() =>
                                                     
 do
 {
-    nombreetage();
-    storednumber=localStorage.getItem('N');
+    nombreetage();                                         
+    storednumber=localStorage.getItem('N');               /*repetition de la demande si nombre d étage > 25*/ 
     
 }while (storednumber > 25);
 
-console.log(storednumber);
+
+console.log(storednumber);          /*afficher le nombre d'étage saisi*/ 
 
 
-const pyramid = n =>
+const pyramid = n =>                  /*fonction qui construit le pyramide et l'affiche*/ 
 
 {
     for (let i =1; i<=n; i++) {
         let string= '';
      
-        for (let j =1; j<=2*n-1; j++) {
-        if(j>=n-i+1 && j<= n+i-1) {string += '#';}
-        else {string+= ' ';}}
+        for (let j =1; j<=2*n-1; j++) { 
+
+         if (j >= n-i+1 && j <= n+i-1) {string += '#';} else {string += ' ';}}
+    
         
         console.log(string);  
     } 
@@ -37,9 +39,7 @@ const pyramid = n =>
  
 }
 
-
-
-pyramid (storednumber);
+pyramid (storednumber);              /*appel de la fonction pyramid*/ 
 
 
 
