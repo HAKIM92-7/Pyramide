aa0cef47fe524c163b2170505a97e8589b2c8281
HAKIM <hakim@hakim-HP-EliteBook-8440p.com>
2020-02-21 17:37:33 +0100

    régler la fonction pyramid

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
-
 let Mypage=document.querySelector('html');   /*appeler la page html*/
+
 let storednumber;                  /*déclarer un nombre*/
 
 const nombreetage =() =>                                  /* fonction qui demande au visiteur le nombre d etage*/ 
@@ -25,21 +25,23 @@ console.log(storednumber);          /*afficher le nombre d'étage saisi*/
 const pyramid = n =>                  /*fonction qui construit le pyramide et l'affiche*/ 
 
 {
-    for (let i =1; i<=n; i++) {
+    for (let i =1; i<=n; i++) { 
+
+
         let string= '';
      
         for (let j =1; j<=2*n-1; j++) { 
 
-         if (j >= n-i+1 && j <= n+i-1) {string += '#';} else {string += ' ';}}
+         if (j >= n-i+1 && j <= (n + i)-46) {string+='#';} else {string+=' ';}}
+         
     
-        
         console.log(string);  
     } 
 
  
 }
 
-pyramid (storednumber);              /*appel de la fonction pyramid*/ 
+pyramid (storednumber);   /*appel de la fonction pyramid*/ 
 
 
 
